6cb0304404d4190180c0f231ddd0dd13365ad3ab
vleadadmin <admin@vlabs.ac.in>
2026-02-03 21:30:56 +0000

    https://virtual-labs.github.io/exp-binary-input-channels-iiith click on the link to test your code.

---
+++ simulation/js/binary_symmetric_channel.js
@@ -0,0 +1,291 @@
+const codewords = [
+    {codeword: [0, 0, 0, 0],
+    },{codeword: [0, 0, 0, 1],
+    },{codeword: [0, 0, 1, 0],
+    },{codeword: [0, 0, 1, 1],
+    },{codeword: [0, 1, 0, 0],
+    },{codeword: [0, 1, 0, 1],
+    },{codeword: [0, 1, 1, 0],
+    },{codeword: [0, 1, 1, 1],
+    },{codeword: [1, 0, 0, 0],
+    },{codeword: [1, 0, 0, 1],
+    },{codeword: [1, 0, 1, 0],
+    },{codeword: [1, 0, 1, 1],
+    },{codeword: [1, 1, 0, 0],
+    },{codeword: [1, 1, 0, 1],
+    },{codeword: [1, 1, 1, 0],
+    },{codeword: [1, 1, 1, 1],}
+];
+
+const eps = "\u03B5";
+
+const wrongans = [
+    {wans: [0, eps , 0, eps],
+    },{wans: [1.24, 0.82, 1.78, 2.12],
+    },{wans: [1.09, 0.09, 2.13, 0.46],
+    },{wans: [0, 0, 1, eps],
+    },{wans: [0, 1, eps, 0],
+    },{wans: [0.24, 0.79, 0.98, 1.45],
+    },{wans: [eps, 1, 1, 0],
+    },{wans: [1.22, 1.35, 0.52, 0.97],
+    },{wans: [1, eps, 0, 0],
+    },{wans: [1, 0, eps, 1],
+    },{wans: [1, 0, eps, 1],
+    },{wans: [1, eps, 0, 1],
+    },{wans: [1, 1, eps, 1],}
+];
+
+var sentcodeword = Array.from(selectRandomCodeword().codeword);
+document.getElementById("sentCodeword").innerHTML = sentcodeword;
+document.getElementById("newCodeword").innerHTML = sentcodeword;
+
+
+function selectRandomCodeword() {
+    const randomIndex = Math.floor(Math.random() * codewords.length);
+    return codewords[randomIndex];
+}
+function selectRandomWans() {
+    const randomwIndex = Math.floor(Math.random() * wrongans.length);
+    return wrongans[randomwIndex];
+}
+
+function formatMatrix(matrix) {
+    return "[" + matrix + "]";
+}
+
+var buttonIds = ["a1", "a2", "a3", "a4","a5", "a6", "a7", "a8"];
+
+function reset(){
+    const bscp1 = document.getElementById("bscp1");
+    const bscp2 = document.getElementById("bscp2");
+    const bscobs1 = document.getElementById("bscobs1");
+    const bscobs2 = document.getElementById("bscobs2");
+    const bscobs12 = document.getElementById("bscobs12");
+
+    bscobs1.innerHTML = "";
+    bscobs12.innerHTML = "";
+    bscobs2.innerHTML = "";
+
+    bscp1.style.display = "block";
+    bscp2.style.display = "none";
+    document.getElementById("nextpagebutton").style.display = "none";
+    
+
+    buttonIds.forEach(function(buttonId) {
+        // Get the button element
+        var buttons = document.getElementById(buttonId);
+        
+        // Set the random number as the button's text
+        buttons.style.backgroundColor = "";
+    });
+
+    var arrsplice = [];
+    var codebuttons = [];
+    var wansbuttons = [];
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+shuffleArray(codewords);
+shuffleArray(wrongans);
+
+
+const arrsplice = buttonIds.slice();
+
+// Shuffle the array using the Fisher-Yates algorithm
+for (let i = arrsplice.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arrsplice[i], arrsplice[j]] = [arrsplice[j], arrsplice[i]];
+}
+
+codebuttons = arrsplice.slice(0, 3);
+wansbuttons = arrsplice.slice(3, 8);
+
+var corrcw = [];
+var wroncw = [];
+
+window.onload = function() {
+    var i = 0;
+    var j = 0;
+    wansbuttons.forEach(function(wansbutton) {
+        
+        
+        var buttonw = document.getElementById(wansbutton);
+        randw = wrongans[i].wans;
+        wroncw.push(randw)
+        
+        // Set the random number as the button's text
+        buttonw.innerHTML = '<span style="font-size: 20px; font-weight: bold; color: black;">' + formatMatrix(randw) + '</span>';
+        i = i + 1;
+        console.log(randw);
+    });
+
+    codebuttons.forEach(function(codebutton) {
+        // Get the button element
+        
+        var button = document.getElementById(codebutton);
+        rand = codewords[j].codeword;
+        corrcw.push(rand)
+        
+        // Set the random number as the button's text
+        button.innerHTML = '<span style="font-size: 20px; font-weight: bold; color: black;">' + formatMatrix(rand) + '</span>';
+        j = j + 1;
+        console.log(rand);
+    });
+};
+
+function nextpage() {
+    const bscp1 = document.getElementById("bscp1");
+    const bscp2 = document.getElementById("bscp2");
+    const bscobs1 = document.getElementById("bscobs1");
+    const bscobs2 = document.getElementById("bscobs2");
+    const bscobs12 = document.getElementById("bscobs12");
+
+    bscobs1.innerHTML = "";
+    bscobs12.innerHTML = "";
+    bscobs2.innerHTML = "";
+
+    bscp1.style.display = "none";
+    bscp2.style.display = "block";
+
+    document.getElementById("codeword1").innerHTML = formatMatrix(corrcw[0].toString());
+    document.getElementById("codeword2").innerHTML = formatMatrix(corrcw[1].toString());
+    document.getElementById("codeword3").innerHTML = formatMatrix(corrcw[2].toString());
+}
+
+function prevpage() {
+    const bscp1 = document.getElementById("bscp1");
+    const bscp2 = document.getElementById("bscp2");
+    const bscobs1 = document.getElementById("bscobs1");
+    const bscobs2 = document.getElementById("bscobs2");
+    const bscobs12 = document.getElementById("bscobs12");
+
+    bscobs1.innerHTML = "";
+    bscobs12.innerHTML = "";
+    bscobs2.innerHTML = "";
+
+    bscp1.style.display = "block";
+    bscp2.style.display = "none";
+}
+
+var array1 = [];
+
+function change(id) {
+    const element = document.getElementById(id);
+
+    if (element.style.backgroundColor === "rgb(26, 255, 0)") {
+        element.style.backgroundColor = "rgb(200, 200, 200)";
+            const index = array1.indexOf(id);
+            if (index !== -1) {
+                array1.splice(index, 1);
+            }
+}   else {
+        element.style.backgroundColor = "rgb(26, 255, 0)";
+        array1.push(id);
+    }
+}
+
+function check1() {
+    const bscobs1 = document.getElementById("bscobs1");
+    const bscobs12 = document.getElementById("bscobs12");
+    const selectedCodewords = document.querySelectorAll('#bscp1 .code-word button[style="background-color: rgb(26, 255, 0);"]');
+
+    if (selectedCodewords.length == 0) {
+        bscobs1.innerHTML = "No output codeword has been selected. Kindly choose the codewords by clicking on them.";
+        bscobs1.style.color = "black";
+    }
+    else {
+        const selectedIds = Array.from(selectedCodewords).map(button => button.id);
+        const correctIds = codebuttons.slice();
+        const isCorrect = correctIds.every(id => selectedIds.includes(id)) && selectedIds.length === correctIds.length;
+
+
+        if (isCorrect) {
+            bscobs1.innerHTML = '{' + "[" + corrcw[0] + "];" + "[" +  corrcw[1] + "];" + "[" +  corrcw[2] + ']}';
+            bscobs12.innerHTML = "<b>Correct! The above selected output vectors are indeed the right possible outputs for the given codeword.</b>";
+            bscobs1.style.color = "green";
+            document.getElementById("nextpagebutton").style.display = "initial";
+        } else {
+            bscobs1.innerHTML = "<b>Kindly check as to what the correct output vectors could be by going through the theory.</b>";
+            bscobs1.style.color = "red";
+            bscobs12.innerHTML = "";
+        }
+    }
+}
+
+function compareArrays(arr1, arr2) {
+    var p = 0;
+    var p1 = 0;
+
+    for(let i=0; i<arr1.length; i++) {
+        if(arr1[i] != arr2[i]) {
+            p++;
+        }
+        else {
+            p1++;
+        }
+    }
+    return [p,p1];      
+}
+
+
+
+
+function check2(){
+
+    let [a11,a12] = compareArrays(corrcw[0], sentcodeword);
+    let [a21,a22] = compareArrays(corrcw[1], sentcodeword);
+    let [a31,a32] = compareArrays(corrcw[2], sentcodeword);
+
+    
+    var p11 = parseInt(document.getElementById("p11").value);
+    var p12 = parseInt(document.getElementById("p12").value);
+    var p21 = parseInt(document.getElementById("p21").value);
+    var p22 = parseInt(document.getElementById("p22").value);
+    var p31 = parseInt(document.getElementById("p31").value);
+    var p32 = parseInt(document.getElementById("p32").value);
+
+    if (isNaN(p11) || isNaN(p12) || isNaN(p21) || isNaN(p22) || isNaN(p31) || isNaN(p32) ||
+        p11 < 0 || p11 > 9 || p12 < 0 || p12 > 9 || p21 < 0 || p21 > 9 ||
+        p22 < 0 || p22 > 9 || p31 < 0 || p31 > 9 || p32 < 0 || p32 > 9) {
+        document.getElementById("bscobs2").innerHTML = "Please enter valid positive integers below 9 into the boxes.";
+        document.getElementById("bscobs2").style.color = "black";
+        return;
+    }
+
+    switch (true) {
+            
+        case (a11 === p11 && a12 === p12 && a21 === p21 && a22 === p22 && a31 === p31 && a32 === p32):
+            document.getElementById("bscobs2").innerHTML = "Correct Answer!";
+            document.getElementById("bscobs2").style.color = "green";
+            break;
+
+        case (a11 !== p11 || a12 !== p12):
+            document.getElementById("bscobs2").innerHTML = "Kindly check the hamming distance between the input codeword and the output vector for part (a) again";
+            document.getElementById("bscobs2").style.color = "blue";
+            break;
+
+        case (a21 !== p21 || a22 !== p22):
+            document.getElementById("bscobs2").innerHTML = "Kindly check the hamming distance between the input codeword and the output vector for part (b) again";
+            document.getElementById("bscobs2").style.color = "blue";
+            break;
+    
+        case (a31 !== p31 || a32 !== p32):
+            document.getElementById("bscobs2").innerHTML = "Kindly check the hamming distance between the input codeword and the output vector for part (c) again";
+            document.getElementById("bscobs2").style.color = "blue";
+            break;
+    
+        default:
+            document.getElementById("bscobs2").innerHTML = "Incorrect Answer! <br> Please go through the Instructions, and try again.";
+            document.getElementById("bscobs2").style.color = "red";
+            break;
+    }
+
+}
